991268824436b0351df32393f73d05bcb70acbeb
tmrd993 <tmerd@hotmail.de>
2021-08-05 19:43:57 +0200

    complete initial project setup

---
+++ shorty-server/shorty-server/src/main/java/com/timucin/shortyserver/ShortyServerApplication.java
@@ -0,0 +1,13 @@
+package com.timucin.shortyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortyServerApplication.class, args);
+	}
+
+}
---
+++ shorty-server/shorty-server/src/test/java/com/timucin/shortyserver/ShortyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timucin.shortyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
